0731ac110a14728bf972eeb171bf2fcf9a36d325
Nana <iamthenanah@gmail.com>
2026-08-04 14:27:07 +0330

    Add edit expense feature and improve validation

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ import json
 from datetime import datetime
 
 categories = {
-    '1':'Food 🍕',
-    '2':'Drinks ☕',
-    '3':'Transportation 🚕',
-    '4':'Education 📚',
-    '5':'Entertainment 🎞️',
-    '6':'Bills 🛋️',
-    '7':'Health 🍎',
-    '8':'Other 📦',
-   
+    '1': 'Food 🍕',
+    '2': 'Drinks ☕',
+    '3': 'Transportation 🚕',
+    '4': 'Education 📚',
+    '5': 'Entertainment 🎞️',
+    '6': 'Bills 🛋️',
+    '7': 'Health 🍎',
+    '8': 'Other 📦',
 }
 
 
@@ -18,9 +17,10 @@ def save_expenses(expenses):
     with open('expenses.json', 'w', encoding='utf-8') as file:
         json.dump(expenses, file, indent=4, ensure_ascii=False)
 
+
 def load_expenses():
     try:
-        with open('expenses.json','r', encoding='utf-8') as file:
+        with open('expenses.json', 'r', encoding='utf-8') as file:
             return json.load(file)
     except (FileNotFoundError, json.JSONDecodeError):
         return []
@@ -32,16 +32,17 @@ def format_datetime(datetime_string):
     date = datetime.fromisoformat(datetime_string)
     return date.strftime('%d %B %Y - %H:%M')
 
+
 def add_expense(expenses):
     item = input('Expense name: ')
     if not item.strip():
         print('Expense name cannot be empty!')
-        return 
+        return
     try:
         amount = float(input('Amount: '))
-        if amount <= 0 :
+        if amount <= 0:
             print('Amount must be greater than zero!')
-            return 
+            return
     except ValueError:
         print('Invalid amount!')
         return
@@ -58,7 +59,7 @@ def add_expense(expenses):
 
     expense = {
         'name': item,
-        'amount' : amount,
+        'amount': amount,
         'category': categories[category_choice],
         'datetime': current_time.isoformat()
         # 'datetime': current_time.strftime('%Y-%m-%d %H:%M')
@@ -75,39 +76,44 @@ def show_expenses(expenses):
     else:
         print('\nYour expenses:')
         for expense in expenses:
-            print(f"name: {expense['name']} - amount: ${expense['amount']:.2f} - category: {expense['category']} - time:{format_datetime(expense['datetime'])} ")
+            print(
+                f"name: {expense['name']} - amount: ${expense['amount']:.2f} - category: {expense['category']} - time:{format_datetime(expense['datetime'])} ")
 
 
 def total_expense(expenses):
-        if not expenses:
-            print('No expenses yet!')
-        else:
-            total = sum(expense['amount'] for expense in expenses)
-       
-            print(f'Total expense: ${total:.2f} 💵')
-    
+    if not expenses:
+        print('No expenses yet!')
+    else:
+        total = sum(expense['amount'] for expense in expenses)
+
+        print(f'Total expense: ${total:.2f} 💵')
+
+
 def delete_expense(expenses):
     if not expenses:
         print('No expenses!')
         return
 
-    print('\nChoose expense to delete: ')        
+    print('\nChoose expense to delete: ')
     for index, expense in enumerate(expenses, start=1):
-        print(f"{index}. {expense['name']} - ${expense['amount']:.2f} - {expense['category']}")
+        print(
+            f"{index}. {expense['name']} - ${expense['amount']:.2f} - {expense['category']}")
     try:
         deleted_item = int(input('Choose expense to delete: '))
     except ValueError:
-        print('Invalid number!')    
+        print('Invalid number!')
         return
     if deleted_item < 1 or deleted_item > len(expenses):
         print('Expense not found!')
         return
-    deleted = expenses.pop(deleted_item - 1 )
+    deleted = expenses.pop(deleted_item - 1)
     save_expenses(expenses)
     print('Your expenses now:')
     for expense in expenses:
-        print(f"{expense['name']} - ${expense['amount']:.2f} - {expense['category']}")
+        print(
+            f"{expense['name']} - ${expense['amount']:.2f} - {expense['category']}")
     print(f"\n{deleted['name']} deleted successfully!")
+
 
 def search_expense(expenses):
     if not expenses:
@@ -120,7 +126,8 @@ def search_expense(expenses):
 
     for expense in expenses:
         if keyword in expense['name'].lower():
-            print(f"{expense['name']} - ${expense['amount']:.2f} - {expense['category']} - {format_datetime(expense['datetime'])}")
+            print(
+                f"{expense['name']} - ${expense['amount']:.2f} - {expense['category']} - {format_datetime(expense['datetime'])}")
             found = True
     if not found:
         print('No matching expense found!')
@@ -137,13 +144,13 @@ if updated:
     save_expenses(expenses)
 
 
-while True: 
+while True:
     print('\n==== Expense Tracker ====')
     print('1. Add Expense')
     print('2. Show Expense')
     print('3. Total Expense')
-    print('4. Delete Expense')    
-    print('5. Search Expense')    
+    print('4. Delete Expense')
+    print('5. Search Expense')
     print('6. Exit')
 
     choice = input('Choose: ')
